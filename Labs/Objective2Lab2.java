88c5ea0d6349d072ab0a7389ff8d7bb44fa4b03b
Alex Trill <alextrillaet@gmail.com>
2022-01-28 11:26:19 -0700

    complete Objectie2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,15 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Alex Trill";
+    int age = 26;
+    String state = "Colorado";
+    System.out.print(" Name: ");
+    System.out.print(name);
+    System.out.print(" Age: ");
+    System.out.print(age);
+    System.out.print(" State: ");
+    System.out.print(state);
+
+  }
+
+}
