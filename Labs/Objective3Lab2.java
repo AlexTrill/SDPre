5e48db30a5eeb45a6290c3f5bf27aa228d40027b
Alex Trill <alextrillaet@gmail.com>
2022-01-28 21:39:37 -0700

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+    public static void main(String[] args) {
+      double side1, side2, hypotenuse;
+      side1 = 15.0;
+      side2 = 20.0;
+      hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+      System.out.print("The hypotonuse of a triangle with sides 15.00 and 20.00 is ");
+      System.out.println(hypotenuse);
+
+    }
+}
